df8fb00abd7035702913ebe78eb1dbed3e8c14a3
Anna <33619352+annty2@users.noreply.github.com>
2020-04-26 17:52:04 +0300

    section 3- multiplayer with split screens

---
+++ Section3/Two Players/Assets/Scenes/scripts/cube2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube2 : MonoBehaviour
+
+{
+
+    float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        moveSpeed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal2") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical2") * Time.deltaTime);
+
+    }
+
+}
+
