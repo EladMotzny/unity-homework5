ed307225374653e1e569974f5dcf1e333c30af98
Anna <33619352+annty2@users.noreply.github.com>
2020-04-27 13:51:33 +0300

    sphere goes in circles

---
+++ Section1/Moving Objects/Assets/Scripts/circleSphere.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class circleSphere : MonoBehaviour
+{
+
+    float timeCounter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        timeCounter += Time.deltaTime;
+
+
+        float x = 13f;
+        float y = 1;
+        float z = 0;
+
+        if (Input.GetKey("left"))
+        {
+            x = Mathf.Cos(timeCounter) * 4f + 13f;
+            z = Mathf.Sin(timeCounter) * 4f;
+            transform.position = new Vector3(x, y, z);
+        }
+
+        if (Input.GetKey("right"))
+        {
+            x = Mathf.Cos(timeCounter) * 4f + 13f;
+            z = Mathf.Sin(-timeCounter) * 4f;
+            transform.position = new Vector3(x, y, z);
+        }
+
+        
+    }
+}
