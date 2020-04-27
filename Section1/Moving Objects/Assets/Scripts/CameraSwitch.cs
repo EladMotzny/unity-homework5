7f15110fb802e367cd527b7acc348268202266aa
Anna <33619352+annty2@users.noreply.github.com>
2020-04-27 18:55:01 +0300

    Add cameras to watch each object

---
+++ Section1/Moving Objects/Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+
+    public Camera[] cams;
+
+    public void camPendulum()
+    {
+        cams[0].enabled = true;
+        cams[1].enabled = false;
+        cams[2].enabled = false;
+        cams[3].enabled = false;
+    }
+    public void camRotate()
+    {
+        cams[0].enabled = false;
+        cams[1].enabled = true;
+        cams[2].enabled = false;
+        cams[3].enabled = false;
+    }
+
+    public void camCircle()
+    {
+        cams[0].enabled = false;
+        cams[1].enabled = false;
+        cams[2].enabled = true;
+        cams[3].enabled = false;
+    }
+
+    public void camRescale()
+    {
+        cams[0].enabled = false;
+        cams[1].enabled = false;
+        cams[2].enabled = false;
+        cams[3].enabled = true;
+    }
+
+
+
+}
--- Section1/Moving Objects/Assets/Scripts/circleSphere.cs
+++ Section1/Moving Objects/Assets/Scripts/circleSphere.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 public class circleSphere : MonoBehaviour
 {
 
-    private Vector3 target = new Vector3(14f,1f, 0.0f);
+    private Vector3 target = new Vector3(21.9f,2.8f,-66.8f);
+    
 
     // Update is called once per frame
     void Update()
     {
 
 
-
         if (Input.GetKey("left"))
         {
-            transform.RotateAround(target, Vector3.up, 30 * Time.deltaTime);
+            transform.RotateAround(target, Vector3.up, 30 * Time.deltaTime *3f );
         }
         if (Input.GetKey("right"))
         {
-            transform.RotateAround(target, Vector3.up, -30 * Time.deltaTime);
+            transform.RotateAround(target, Vector3.up, -30 * Time.deltaTime *3f );
         }
 
 
