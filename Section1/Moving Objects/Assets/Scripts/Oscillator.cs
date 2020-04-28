5fee3d97bc601f32107f30dc83bb9d04bfd3adec
Anna <33619352+annty2@users.noreply.github.com>
2020-04-28 16:37:10 +0300

    add pendulum

---
+++ Section1/Moving Objects/Assets/Scripts/Oscillator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator: MonoBehaviour
+{
+    public float angle = 90.0f;
+    public float speed = 1.5f;
+    Quaternion qStart, qEnd;
+    private float startTime;
+    void Start()
+    {
+        qStart = Quaternion.AngleAxis(angle, Vector3.forward);
+        qEnd = Quaternion.AngleAxis(-angle, Vector3.forward);
+    }
+    void Update()
+    {
+        
+            startTime += Time.deltaTime;
+            transform.rotation = Quaternion.Lerp(qStart, qEnd, (Mathf.Sin(startTime * speed + Mathf.PI / 2) + 1.0f) / 2.0f);
+        
+    }
+}
