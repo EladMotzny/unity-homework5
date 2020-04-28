aa16770b07315facdbb17b2dc237275edf902c9e
Elad Motzny <emotzny@gmail.com>
2020-04-28 20:37:00 +0300

    Added coin collection and sound

--- Section2/two/MiniMap/Assets/Scripts/Move.cs
+++ Section2/two/MiniMap/Assets/Scripts/Move.cs
@@ -7,11 +7,14 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    [Tooltip("Movement speed is in m/s")] [SerializeField] float moveSpeed = 1f;
+    [Tooltip("Change to increase/decrease movement speed")] [SerializeField] float moveSpeed = 1f;
+    [Tooltip("Change to increase/decrease jump height")] [SerializeField] float jumpHeight = 5;
+
+    public AudioSource coinSound;
     // Start is called before the first frame update
     void Start()
     {
-        
+        coinSound = GetComponent<AudioSource> ();
     }
 
     // Update is called once per frame
@@ -23,9 +26,17 @@ public class Move : MonoBehaviour
         
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Coin")) {
+            coinSound.Play();
+            Destroy(other.gameObject);
+        }
+    }
+
     void Jump() {
         if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
         }
     }
 }
