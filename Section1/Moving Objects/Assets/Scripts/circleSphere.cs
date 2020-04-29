bfe49641e11a1ef15db1af946249e112c5c59263
Anna <33619352+annty2@users.noreply.github.com>
2020-04-29 13:44:37 +0300

    add speed control to the circle

--- Section1/Moving Objects/Assets/Scripts/circleSphere.cs
+++ Section1/Moving Objects/Assets/Scripts/circleSphere.cs
@@ -6,7 +6,8 @@ public class circleSphere : MonoBehaviour
 {
 
     private Vector3 target = new Vector3(21.9f,2.8f,-66.8f);
-    
+    [Tooltip("Movement speed in meters per second")] [SerializeField] float speed = 100f;
+
 
     // Update is called once per frame
     void Update()
@@ -15,11 +16,11 @@ public class circleSphere : MonoBehaviour
 
         if (Input.GetKey("left"))
         {
-            transform.RotateAround(target, Vector3.up, 30 * Time.deltaTime *3f );
+            transform.RotateAround(target, Vector3.up, speed * Time.deltaTime );
         }
         if (Input.GetKey("right"))
         {
-            transform.RotateAround(target, Vector3.up, -30 * Time.deltaTime *3f );
+            transform.RotateAround(target, Vector3.up, -speed * Time.deltaTime );
         }
 
 
