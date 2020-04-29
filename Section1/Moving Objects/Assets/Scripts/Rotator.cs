7dd3551f1c08ec1d275705cc18be8b0fc1157f3d
Anna <33619352+annty2@users.noreply.github.com>
2020-04-29 13:33:18 +0300

    add speed control to the rotator

--- Section1/Moving Objects/Assets/Scripts/Rotator.cs
+++ Section1/Moving Objects/Assets/Scripts/Rotator.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
+
+    [Tooltip("Movement speed in meters per second")] [SerializeField] float speed = 1f;
     // Update is called once per frame
     void Update()
     {
 
-        transform.Rotate(new Vector3(0f, 1f, 0f));
+        transform.Rotate(new Vector3(0f, speed, 0f));
         
     }
 }
